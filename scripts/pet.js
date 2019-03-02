11aa2bdc6dbb7bbdea2aaa8efeb71e63504852ac
Michael Yankura <myankura210@gmail.com>
2019-03-01 22:23:35 -0600

    The code runs in the console. Pushing completed representing-your-pet exercise to github.

---
+++ scripts/pet.js
@@ -0,0 +1,18 @@
+const pet = {
+    name : "Monty",
+    species : "Python",
+    nicknames : ["Danger Noodle", "Snek", "Monty the Python"],
+    age : 3
+}
+const pet2 = {
+    name : "John Wayne",
+    species : "Ferret",
+    nicknames : ["The Duke", "JW", "Fuzzy"],
+    age : 4
+}
+const pet3 = {
+    name : "Clint Eastwood",
+    species : "Ferret",
+    nicknames : ["Blondie", "Joe (A Fistful of Dollars) Manco", "Wuzzy"],
+    age : 3
+}
